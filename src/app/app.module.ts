5406d424290275dd56653abeee910a12ca3670c9
jackfusion <kendewitt@yftg.ca>
2021-09-09 12:20:49 -0500

    fixed error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './compents/homepage/homepage.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 import { ProductComponent } from './components/product/product.component';
 import { ProductsComponent } from './components/products/products.component';
 import { ProductPageComponent } from './components/product-page/product-page.component';
